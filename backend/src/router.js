b6746a76f74ddd60c47057850fb1eefa8679adbf
Lars Poetzel <lars.poetzel@gmail.com>
2023-01-02 10:14:00 +0100

    used uuid to make same file name possible

--- backend/src/router.js
+++ backend/src/router.js
@@ -6,6 +6,8 @@ const fs = require("fs");
 
 const upload = multer({ dest: "uploads/" });
 
+const { v4: uuidv4 } = require("uuid");
+
 const router = express.Router();
 
 const itemControllers = require("./controllers/itemControllers");
@@ -15,11 +17,15 @@ router.post("/api/avatar", upload.single("avatar"), (req, res) => {
 
   const { filename } = req.file;
 
-  fs.rename(`uploads/${filename}`, `uploads/${originalname}`, (err) => {
-    if (err) throw err;
+  fs.rename(
+    `uploads/${filename}`,
+    `uploads/${uuidv4()}-${originalname}`,
+    (err) => {
+      if (err) throw err;
 
-    res.send("File uploaded");
-  });
+      res.send("File uploaded");
+    }
+  );
 });
 router.get("/items", itemControllers.browse);
 router.get("/items/:id", itemControllers.read);
