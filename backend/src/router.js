75205006bdb9834b5245caf4f3d439bfb9f3a041
Lars Poetzel <lars.poetzel@gmail.com>
2023-01-02 10:10:18 +0100

    rename upload file to own name

--- backend/src/router.js
+++ backend/src/router.js
@@ -2,6 +2,8 @@ const express = require("express");
 
 const multer = require("multer");
 
+const fs = require("fs");
+
 const upload = multer({ dest: "uploads/" });
 
 const router = express.Router();
@@ -9,7 +11,15 @@ const router = express.Router();
 const itemControllers = require("./controllers/itemControllers");
 
 router.post("/api/avatar", upload.single("avatar"), (req, res) => {
-  res.send("File uploaded");
+  const { originalname } = req.file;
+
+  const { filename } = req.file;
+
+  fs.rename(`uploads/${filename}`, `uploads/${originalname}`, (err) => {
+    if (err) throw err;
+
+    res.send("File uploaded");
+  });
 });
 router.get("/items", itemControllers.browse);
 router.get("/items/:id", itemControllers.read);
