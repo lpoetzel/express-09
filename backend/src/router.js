dec24e98e15456500c0c650364ee8c5a898d425e
Lars Poetzel <lars.poetzel@gmail.com>
2023-01-02 09:54:42 +0100

    post upload avatar route

--- backend/src/router.js
+++ backend/src/router.js
@@ -1,9 +1,16 @@
 const express = require("express");
 
+const multer = require("multer");
+
+const upload = multer({ dest: "uploads/" });
+
 const router = express.Router();
 
 const itemControllers = require("./controllers/itemControllers");
 
+router.post("/api/avatar", upload.single("avatar"), (req, res) => {
+  res.send("File uploaded");
+});
 router.get("/items", itemControllers.browse);
 router.get("/items/:id", itemControllers.read);
 router.put("/items/:id", itemControllers.edit);
