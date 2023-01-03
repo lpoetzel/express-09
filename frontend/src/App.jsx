a7921cd81de68711b88b5635561d30df91f5401e
Lars Poetzel <lars.poetzel@gmail.com>
2023-01-03 16:48:52 +0100

    frontend for upload

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,24 @@
-import Home from "./pages/Home";
-
+import axios from "axios";
+import React, { useRef } from "react";
 import "./App.css";
 
 function App() {
+  const inputRef = useRef();
+  function hSubmit(evt) {
+    evt.preventDefault();
+
+    const formData = new FormData();
+    formData.append("avatar", inputRef.current.files[0]);
+
+    axios.post("http://localhost:5000/api/avatar", formData);
+  }
   return (
     <div className="App">
-      <Home />
-      <p>coucou</p>
+      <form encType="multipart/form-data" onSubmit={hSubmit}>
+        <input type="file" name="avatar" ref={inputRef} />
+
+        <button type="submit">Send!</button>
+      </form>
     </div>
   );
 }
